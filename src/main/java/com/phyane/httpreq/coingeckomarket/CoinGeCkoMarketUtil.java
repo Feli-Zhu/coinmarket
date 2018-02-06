a049bc480acea32c26ae99723694d1edd9903fc3
Feli-Zhu <zfyj_2004"163.com>
2018-02-06 11:10:09 +0800

    文件完善

--- src/main/java/com/phyane/httpreq/coingeckomarket/CoinGeCkoMarketUtil.java
+++ src/main/java/com/phyane/httpreq/coingeckomarket/CoinGeCkoMarketUtil.java
@@ -1,9 +1,21 @@
 package com.phyane.httpreq.coingeckomarket;
 
+import java.util.List;
+
+import com.phyane.comm.JsonUtil;
+import com.phyane.param.CoinMarketParam;
+import com.phyane.spider.HtmlParaser;
+import com.phyane.spider.UrlParaser;
+
 public class CoinGeCkoMarketUtil {
 	
-	public void getCoinList() {
-		
+	public static void main(String[] args) throws Exception {
+		String url = "https://www.coingecko.com/en/crypto_index/crix";
+		UrlParaser wcr = new UrlParaser();
+		HtmlParaser hp = new HtmlParaser();
+		String html = wcr.paraseUrl2Html(url);
+		List<CoinMarketParam> list = hp.paraseCoinGeCkoMarketHtml(html);
+		String json = JsonUtil.obj2Json(list);
+		System.out.println(json);
 	}
-
 }
